5ec9e059b67baa46abd38ab4d5fc9d41d2c60c0f
lepouletsuisse <xxpoulletxx@gmail.com>
2015-10-13 14:46:23 +0200

    Set workspace

---
+++ POO/Labo03/src/Point.java
@@ -0,0 +1,17 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : <nn>
+ Fichier     : Point.java
+ Auteur(s)   : Samuel Darcey
+ Date        : 13.10.2015
+
+ But         : <à compléter>
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : jdk1.8.0_60
+ -----------------------------------------------------------------------------------
+*/
+
+public class Point {
+}
